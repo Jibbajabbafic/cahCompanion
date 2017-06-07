57314a45172026a87ecfd0de2ddd5da1242612a5
Jibbajabba <Jibbajabbafic@users.noreply.github.com>
2017-06-07 11:02:19 +0100

    cahCompanion
    Little webpage to help you play Cards Against Humanity. Keeps track of score and draws black cards

---
+++ cards.js
@@ -0,0 +1,243 @@
+
+function loadPack(packObj, cardArry) {
+	//Function to load a pack into specified array
+	if (packObj.loaded) {
+		console.log("WARNING: " + packObj.name + " already loaded, skipping!")
+		return;
+	}
+
+	else {
+		var cards = [];
+		var xhttp;
+	  	if (window.XMLHttpRequest) {
+		    //Code for modern browsers
+		    xhttp = new XMLHttpRequest();
+		    } else {
+		    //Code for IE6, IE5
+		    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	  	}
+	  	
+	  	xhttp.onreadystatechange = function() {
+		    if (this.readyState == 4 && this.status == 200) {
+		    	cards = this.responseText.split("\n");
+		    	cardArry.push.apply(cardArry, cards);
+		    	packObj.loaded = true;
+		    	console.log("Loaded " + packObj.name);
+		    }
+		};
+
+		xhttp.open("GET", "/blackCards/" + packObj.path, true);
+		xhttp.send();
+	}
+}
+
+// function packSelect(arry) {
+// 	//Load all enabled packs into
+// 	var loadedPacks = 0;
+
+// 	for (var i = 0; i < packList.length; i++) {
+// 		if (packList[i].enabled == true && packList[i].loaded == true) {
+// 			console.log("WARNING: " + packList[i].name + " already loaded, skipping.");
+// 			break;
+// 		} else if (packList[i].enabled == true && packList[i].loaded == false) {
+// 			loadPack(packList[i], cardPool);
+// 			loadedPacks += 1;
+// 		}
+// 	}
+
+// 	if (loadedPacks > 0 ) console.log("Loaded " + loadedPacks + " packs into the card pool");
+// 	else console.log("WARNING: No packs loaded!");
+// }
+
+function populatePackList(packArry) {
+	//Fill the pack list with the available packs
+	var inputEl, node, element;
+
+	for (var i = 0; i < packArry.length; i++) {
+		inputEl = document.createElement("input");
+		inputEl.setAttribute("type", "checkbox");
+		inputEl.setAttribute("name", "pack");
+		inputEl.setAttribute("value", i);
+
+		node = document.createTextNode(packArry[i].name);
+
+		element = document.getElementById(packArry[i].cat);
+		element.appendChild(inputEl);
+		element.appendChild(node);
+
+		brEl = document.createElement("br");
+		element.appendChild(brEl);
+	}
+}
+
+function changeTab(tabName) {
+	//Switch active tab to element with id tabName
+	var e = document.getElementsByClassName("tab");
+	for (var i = 0; i < e.length; i++) {
+		e[i].style.display = "none";
+	}
+	var x = document.getElementById(tabName);
+	x.style.display = "block";
+}
+
+function menuPack() {
+	//Switch to pack selection menu, populates list with available packs dynamically
+	populatePackList(packList);
+	changeTab("packSelect");
+	console.log("Selecting packs!");
+}
+
+function menuPlayer() {
+	//Switch to player adding menu, add and remove desired number of players
+	changeTab("addPlayers");
+	console.log("Adding players!");
+}
+
+function menuGame() {
+	changeTab("playGame");
+	console.log("Game started!");
+}
+
+// function setPack(packArry, packIndex, bool) {
+// 	//Enable and disable certain packs
+// 	packArry[packIndex].enabled = bool;
+// }
+
+function drawCard(cardArry) {
+	var cardIndex = Math.floor((Math.random() * cardArry.length));
+	console.log("Returning " + cardIndex);
+	return cardArry[cardIndex];
+}
+
+var cardPool = [];
+
+var packList = [
+	{
+		name: "Base Game 1",
+		cat: "base",
+		path: "base1.txt",
+		enabled: true,
+		loaded: false
+	},
+
+	{
+		name: "Base Game 1.6",
+		cat: "base",
+		path: "base1.6.txt",
+		enabled: false,
+		loaded: false
+	},
+
+	{
+		name: "Base Game (UK)",
+		cat: "base",
+		path: "baseUK.txt",
+		enabled: false,
+		loaded: false
+	},
+
+	{
+		name: "Base Game (Australia)",
+		cat: "base",
+		path: "baseAUS.txt",
+		enabled: false,
+		loaded: false
+	},
+
+	{
+		name: "Base Game (Canada)",
+		cat: "base",
+		path: "baseCAN.txt",
+		enabled: false,
+		loaded: false
+	},
+
+	{
+		name: "Base Game (US)",
+		cat: "base",
+		path: "baseUS.txt",
+		enabled: false,
+		loaded: false
+	},		
+
+	{
+		name: "The First Expansion",
+		cat: "expan",
+		path: "expan1.txt",
+		enabled: false,
+		loaded: false
+	},		
+
+	{
+		name: "The Second Expansion",
+		cat: "expan",
+		path: "expan2.txt",
+		enabled: false,
+		loaded: false
+	},		
+
+	{
+		name: "The Third Expansion",
+		cat: "expan",
+		path: "expan3.txt",
+		enabled: false,
+		loaded: false
+	},	
+
+	{
+		name: "The Fourth Expansion",
+		cat: "expan",
+		path: "expan4.txt",
+		enabled: false,
+		loaded: false
+	},		
+
+	{
+		name: "The Fifth Expansion",
+		cat: "expan",
+		path: "expan5.txt",
+		enabled: false,
+		loaded: false
+	},		
+
+	{
+		name: "The Sixth Expansion",
+		cat: "expan",
+		path: "expan6.txt",
+		enabled: false,
+		loaded: false
+	},			
+
+	{
+		name: "The Green Box",
+		cat: "expan",
+		path: "expanGreen.txt",
+		enabled: false,
+		loaded: false
+	},	
+
+	{
+		name: "The Fantasy Pack",
+		cat: "misc",
+		path: "miscFantasy.txt",
+		enabled: false,
+		loaded: false
+	},				
+
+	{
+		name: "The Science Pack",
+		cat: "misc",
+		path: "miscScience.txt",
+		enabled: false,
+		loaded: false
+	},
+
+	{
+		name: "Custom Cards",
+		cat: "misc",
+		path: "custom.txt",
+		enabled: false,
+		loaded: false
+	}
+];
+
---
+++ scores.js
@@ -0,0 +1,135 @@
+var gameScoreboard = [];
+
+function addPlayer(playerName, scoreboard) {
+	//Pushes a new player object onto the chosen scoreboard
+	for (var i = 0; i < scoreboard.length; i++) {
+		if (scoreboard[i].player == playerName) {
+			console.log("WARNING: Player already exists!");
+			return 0;
+		}
+	}
+	var newPlayer = {player: playerName, score: 0}
+	scoreboard.push(newPlayer)
+	console.log("Added " + playerName + " to scoreboard")
+	return 1;
+}
+
+function removePlayer(playerName, scoreboard) {
+	//Removes a player from the scoreboard
+	for (var i = 0; i < scoreboard.length; i++) {
+		if (scoreboard[i].player == playerName) {
+			scoreboard.splice(i,1);
+			console.log("Removed " + playerName + " from scoreboard");
+			return;
+		}
+	}
+	//If player not found show an error (could use try, catch and throw instead)
+	console.log("ERROR: Player " + playerName + " does not exist!");
+}
+
+function addWin(playerName, wins = 1, scoreboard) {
+	//Adds a specified number of wins to specified player's score
+	for (var i = 0; i < scoreboard.length; i++) {
+		if (scoreboard[i].player == playerName) {
+			scoreboard[i].score += wins;
+			console.log("Added " + wins + " wins to " + scoreboard[i].player);
+			return;
+		}
+	}
+	//If player not found show an error (could use try, catch and throw instead)
+	console.log("ERROR: Player does not exist!");
+}
+
+function removeWin(playerName, wins = 1, scoreboard) {
+		//Removes a specified number of points from the specified player's score
+	for (var i = 0; i < scoreboard.length; i++) {
+		if (scoreboard[i].player == playerName) {
+			scoreboard[i].score -= wins;
+			console.log(scoreboard[i].player + " traded in their hand and lost " + wins + " points!");
+			return;
+		}
+	}
+	//If player not found show an error (could use try, catch and throw instead)
+	console.log("ERROR: Player does not exist!");
+}
+
+function sortScores(arry) {
+	//Returns a sorted array, sorted by descending score (highest at arry[0])
+	return arry.sort(
+		function(a,b){return b.score-a.score}
+	)
+}
+
+function printScoreboard(scoreboard) {
+	//Prints the current scores to the console, sorting them and announcing a winner
+
+	//Check if any players in the array
+	if (scoreboard.length == 0) {
+		console.log("ERROR: No players added yet!");
+		return;
+	}
+
+	//Sort the array by score
+	var scoreList = sortScores(scoreboard)
+
+	console.log("Here are the scores so far:")
+
+	//Cycle through each player printing their name and score
+	for (var i = 0; i < scoreboard.length; i++) {
+	 console.log(scoreboard[i].player + ": " + scoreboard[i].score);
+	}
+
+	//List of winner for a tie situation
+	var winnerList = [];
+
+	//Add the person with the highest score
+	winnerList.push(scoreboard[0])
+
+	//Check if any other players have the same score
+	for (var i = 1; i < scoreboard.length; i++) {
+		if(winnerList[0].score == scoreboard[i].score) {
+			winnerList.push(scoreboard[i]);
+		}
+	}
+
+	//Check if there is more than 1 winner and announce a tie
+	if (winnerList.length > 1) {
+		console.log("It's a tie between these dickheads:")
+		for (var i = 0; i < winnerList.length; i++) {
+			console.log(winnerList[i].player)
+		}
+	}
+	//Otherwise announce the winner
+	else console.log("The winner is " + scoreList[0].player + "! \n\n");
+
+}
+
+function resetScoreboard(scoreboard) {
+	//Function to reset scoreboard
+	scoreboard = [];
+}
+
+function resetPlayer(playerName, scoreboard) {
+	//Resets the specified player's score
+	for (var i = 0; i < scoreboard.length; i++) {
+		if (scoreboard[i].player == playerName) scoreboard[i].score = 0;
+	}
+}
+
+// addPlayer("Paul", gameScoreboard);
+// addPlayer("Jasper", gameScoreboard);
+// addPlayer("Dell", gameScoreboard);
+// addPlayer("Hannah", gameScoreboard);
+
+// addWin("Paul", 1, gameScoreboard);
+// addWin("Jasper", 1, gameScoreboard);
+
+// printScoreboard(gameScoreboard);
+
+// addWin("Dell", 4, gameScoreboard);
+
+// printScoreboard(gameScoreboard);
+
+// removeWin("Dell", 1, gameScoreboard);
+
+// printScoreboard(gameScoreboard);
